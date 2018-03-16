13f05e46a92f4230b60f3b82d833417666a41766
Reza <reza@chimaera.my>
2018-03-16 18:21:47 +0800

    Botman dashboard templade separating - 1
    overviews are seperated into cards

---
+++ resources/views/dashboard/overview/card.blade.php
@@ -0,0 +1,14 @@
+<div class="col-md-3">
+    <div class="card p-4">
+        <div class="card-body d-flex justify-content-between align-items-center">
+            <div>
+                <span class="h4 d-block font-weight-normal mb-2">{{ $item['name'] }}</span>
+                <span class="font-weight-light">{{ $item['count'] }}</span>
+            </div>
+
+            <div class="h2 text-muted">
+                <i class="icon icon-people"></i>
+            </div>
+        </div>
+    </div>
+</div>
