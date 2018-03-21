9775bd4930501cc7454ace1def9c26ec6537196a
Reza <reza@chimaera.my>
2018-03-21 11:40:19 +0800

    Admin dashboard seperated and modified
    - cards has been updated and minified

--- resources/views/dashboard/overview/card.blade.php
+++ resources/views/dashboard/overview/card.blade.php
@@ -1,14 +1,24 @@
-<div class="col-md-3">
-    <div class="card p-4">
-        <div class="card-body d-flex justify-content-between align-items-center">
-            <div>
-                <span class="h4 d-block font-weight-normal mb-2">{{ $item['name'] }}</span>
-                <span class="font-weight-light">{{ $item['count'] }}</span>
-            </div>
+@if ($items && count($items) > 0)
+<div class="row">
+    <div class=" col-md-12">
+        <h4>{{ $title }}</h4>
+        <hr/>
+    </div>
+    @foreach($items as $item)
+    <div class="col-md-3">
+        <div class="card p-4">
+            <div class="card-body d-flex justify-content-between align-items-center">
+                <div>
+                    <span class="h4 d-block font-weight-normal mb-2">{{ $item['name'] }}</span>
+                    <span class="font-weight-light">{{ $item['count'] }}</span>
+                </div>
 
-            <div class="h2 text-muted">
-                <i class="icon icon-people"></i>
+                <div class="h2 text-muted">
+                    <i class="icon icon-people"></i>
+                </div>
             </div>
         </div>
     </div>
+    @endforeach
 </div>
+@endif
