22750fdd8c019b168fdabd60f93e9a91822cb424
Reza <reza@chimaera.my>
2018-03-21 15:01:05 +0800

    Master template changes according to Auth status
    - side bar and navbar elements now appears only if user is authorised

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,16 +8,17 @@
     <link href="/css/app.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.16/af-2.2.2/b-1.5.1/b-colvis-1.5.1/b-html5-1.5.1/b-print-1.5.1/fc-3.2.4/fh-3.1.3/r-2.2.1/sl-1.2.5/datatables.min.css"/>
 </head>
-<body class="sidebar-fixed header-fixed">
+<body class="sidebar-fixed header-fixed {{  Auth::check() ? '' : 'sidebar-hidden' }} ">
 <div class="page-wrapper">
     @include('layout.navbar')
     <div class="main-container">
+        @if (Auth::check())
         @include('layout.sidebar')
+        @endif
         @yield('content')
     </div>
 </div>
 <script src="{{asset('js/app.js')}}"></script>
-<!-- <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script> -->
 <script src="{{asset('js/dashboard/chart.min.js')}}"></script>
 <script src="{{asset('js/dashboard/carbon.js')}}"></script>
 <script src="{{asset('js/dashboard/demo.js')}}"></script>
--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -6,57 +6,58 @@
     <a class="navbar-brand" href="#">
         <img src="/images/dashboard/logo.png" alt="logo">
     </a>
+    @if (Auth::check())
+        <a href="#" class="btn btn-link sidebar-toggle d-md-down-none">
+            <i class="fa fa-bars"></i>
+        </a>
 
-    <a href="#" class="btn btn-link sidebar-toggle d-md-down-none">
-        <i class="fa fa-bars"></i>
-    </a>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item d-md-down-none">
+                <a href="#">
+                    <i class="fa fa-bell"></i>
+                    <span class="badge badge-pill badge-danger">5</span>
+                </a>
+            </li>
 
-    <ul class="navbar-nav ml-auto">
-        <li class="nav-item d-md-down-none">
-            <a href="#">
-                <i class="fa fa-bell"></i>
-                <span class="badge badge-pill badge-danger">5</span>
-            </a>
-        </li>
+            <li class="nav-item d-md-down-none">
+                <a href="#">
+                    <i class="fa fa-envelope-open"></i>
+                    <span class="badge badge-pill badge-danger">5</span>
+                </a>
+            </li>
 
-        <li class="nav-item d-md-down-none">
-            <a href="#">
-                <i class="fa fa-envelope-open"></i>
-                <span class="badge badge-pill badge-danger">5</span>
-            </a>
-        </li>
-
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img src="/images/dashboard/avatar-1.png" class="avatar avatar-sm" alt="logo">
-                <span class="small ml-1 d-md-down-none">John Smith</span>
-            </a>
-
-            <div class="dropdown-menu dropdown-menu-right">
-                <div class="dropdown-header">Account</div>
-
-                <a href="#" class="dropdown-item">
-                    <i class="fa fa-user"></i> Profile
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="/images/dashboard/avatar-1.png" class="avatar avatar-sm" alt="logo">
+                    <span class="small ml-1 d-md-down-none">John Smith</span>
                 </a>
 
-                <a href="#" class="dropdown-item">
-                    <i class="fa fa-envelope"></i> Messages
-                </a>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <div class="dropdown-header">Account</div>
 
-                <div class="dropdown-header">Settings</div>
+                    <a href="#" class="dropdown-item">
+                        <i class="fa fa-user"></i> Profile
+                    </a>
 
-                <a href="#" class="dropdown-item">
-                    <i class="fa fa-bell"></i> Notifications
-                </a>
+                    <a href="#" class="dropdown-item">
+                        <i class="fa fa-envelope"></i> Messages
+                    </a>
 
-                <a href="#" class="dropdown-item">
-                    <i class="fa fa-wrench"></i> Settings
-                </a>
+                    <div class="dropdown-header">Settings</div>
 
-                <a href="#" class="dropdown-item">
-                    <i class="fa fa-lock"></i> Logout
-                </a>
-            </div>
-        </li>
-    </ul>
+                    <a href="#" class="dropdown-item">
+                        <i class="fa fa-bell"></i> Notifications
+                    </a>
+
+                    <a href="#" class="dropdown-item">
+                        <i class="fa fa-wrench"></i> Settings
+                    </a>
+
+                    <a href="#" class="dropdown-item">
+                        <i class="fa fa-lock"></i> Logout
+                    </a>
+                </div>
+            </li>
+        </ul>
+    @endif
 </nav>
