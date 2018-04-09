b9ed4ec28ba0df1b3c8090c345092d9d3a5cc806
Reza <reza@chimaera.my>
2018-04-09 14:06:56 +0800

    UI modification
    - super admin role access added

--- resources/views/dashboard/permission/create.blade.php
+++ resources/views/dashboard/permission/create.blade.php
@@ -9,29 +9,50 @@
                   {{ $title }}
               </div>
               <div class="card-body">
-              @can('sa-create-permission')
-               @include('layout.alert')
-                @if(isset($remaining_permissions) && count($remaining_permissions) > 0 ) 
-                <div class="row">
-                  <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <form class="form-inline" method="POST" action="/permission/create">
-                              {{ csrf_field() }}
-                              <select id="category-select" class="form-control mb-3 mr-sm-3 col-1" name="permission">
-                                <option value>Permissions</option>
-                                @foreach($remaining_permissions as $permission)
-                                  <option value={{ $permission }}>{{ $permission }}</option>
-                                @endforeach
-                              </select>
-                                <button type="submit" class="btn btn-primary mb-3">Add permission</button>
-                            </form>
+                @include('layout.alert')
+                  @if(isset($remaining_permissions) && count($remaining_permissions) > 0 ) 
+                    @hasanyrole('super admin')
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="card">
+                              <div class="card-body">
+                                  <form class="form-inline" method="POST" action="/permission/create">
+                                    {{ csrf_field() }}
+                                    <select id="category-select" class="form-control mb-3 mr-sm-3 col-1" name="permission">
+                                      <option value>Permissions</option>
+                                      @foreach($remaining_permissions as $permission)
+                                        <option value={{ $permission }}>{{ $permission }}</option>
+                                      @endforeach
+                                     </select>
+                                    <button type="submit" class="btn btn-primary mb-3">Add permission</button>
+                                  </form>
+                              </div>
+                          </div>
                         </div>
-                    </div>
-                  </div>
-                </div>
+                      </div>
+                    @else 
+                      @can('sa-create-permission')
+                        <div class="row">
+                          <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <form class="form-inline" method="POST" action="/permission/create">
+                                      {{ csrf_field() }}
+                                      <select id="category-select" class="form-control mb-3 mr-sm-3 col-1" name="permission">
+                                        <option value>Permissions</option>
+                                        @foreach($remaining_permissions as $permission)
+                                          <option value={{ $permission }}>{{ $permission }}</option>
+                                        @endforeach
+                                      </select>
+                                      <button type="submit" class="btn btn-primary mb-3">Add permission</button>
+                                    </form>
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                      @endcan
+                  @endhasanyrole
                 @endif
-                @endcan
                 @include('dashboard.permission.list')
               </div>         
           </div>
